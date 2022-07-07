4a811c26223799a7a0c025d911785a018f456cc3
Steve Boardwell <sboardwell@cloudbees.com>
2022-07-07 18:25:54 +0200

    Update CentralPipelineRunner.groovy

--- CentralPipelineRunner.groovy
+++ CentralPipelineRunner.groovy
@@ -1,3 +1,14 @@
 def myQuickConfig = readTrusted 'jetstream.yaml'
 echo "Processing the following config..."
 echo "${myQuickConfig}"
+
+pipeline {
+  agent none
+  stages {
+    stage('Test') {
+      steps {
+        echo "hello"
+      }
+    }
+  }
+}
